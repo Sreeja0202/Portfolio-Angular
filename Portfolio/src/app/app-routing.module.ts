685d48b207172d13841db2df20fad98f4c5f0235
Sreeja0202 <106142975+Sreeja0202@users.noreply.github.com>
2022-10-18 23:01:43 +0530

    2nd update

--- Portfolio/src/app/app-routing.module.ts
+++ Portfolio/src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: PortfolioComponent,
+  },
+  {
+    path: 'portfolio',
+    component: PortfolioComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
